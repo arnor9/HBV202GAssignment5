a21c7ee9597bc257a57f97aad9f1e737d581dc4e
Róbert Jack <robertalex00@hotmail.com>
2023-02-14 12:06:58 +0000

    Verkefnið búið

--- src/main/java/is/hi/hbv202g/ass5/IntStack.java
+++ src/main/java/is/hi/hbv202g/ass5/IntStack.java
@@ -54,6 +54,13 @@ public class IntStack {
 
     // TODO: Add isEmpty() method
 
+    public boolean isEmpty(){
+        if(nextPushLocation == 0){
+            return true;
+        }
+        return false;
+    }
+
     /**
      * Push on int on the stack.
      * @param element The int to be pushed on the stack
--- src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
+++ src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
@@ -1,17 +1,59 @@
 package is.hi.hbv202g.ass5;
 
 import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
 public class IntStackTest {
+    IntStack stack;
+    @Before
+    public void constructNewStackedObject(){
+        stack = new IntStack();
+    }
 
     @Test
     public void testNewStackIsNotFull() {
-        IntStack stack= new IntStack();
-
         assertFalse(stack.isFull());
     }
-
+    @Test
+    public void testFullStackIsFull(){
+        for(int i = 0; i < stack.getCapacity();i++){
+            stack.push(i);
+        }
+        assertTrue(stack.isFull());
+    }
+    @Test
+    public void testFullStackIsAlmostFull(){
+        for(int i = 0; i < stack.getCapacity() -1;i++){
+            stack.push(i);
+        }
+        assertFalse(stack.isFull());
+    }
+    @Test
+    public void testPopReturnsPushValue(){
+        int a = 10;
+        stack.push(a);
+        int b = stack.pop();
+        assertEquals(a,b);
+    }
+    @Test(expected=ArrayIndexOutOfBoundsException.class)
+    public void testPopOnEmptyStack(){
+        stack.pop();
+    }
+    @Test(expected=ArrayIndexOutOfBoundsException.class)
+    public void testPushOnFullStack(){
+        for(int i = 0; i < stack.getCapacity(); i++){
+            stack.push(i);
+        }
+        stack.push(22);
+    }
+    @Test
+    public void testIsEmpty1(){
+        assertTrue(stack.isEmpty());
+    }
+    @Test
+    public void testIsEmpty2(){
+        stack.push(22);
+        assertFalse(stack.isEmpty());
+    }
 }
